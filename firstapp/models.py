afe1f306a677c0fbee91646cf4ba90f66a3c7e15
yyq <2825489825@qq.com>
2023-01-31 22:17:54 +0800

    firstapp.models修改  杨焰琴

--- firstapp/models.py
+++ firstapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class UserInfo(models.Model):
-    id = models.AutoField('学号',primary_key=True)
+    id = models.IntegerField('学号',null=True,blank=True)
     user_name = models.CharField('姓名',max_length=200)
     phase_num = models.CharField('期数', max_length=200)
     direction = models.CharField('方向', max_length=200)
