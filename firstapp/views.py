193198ab025be1b239415482d85649c5c0f4b6f8
angler-loren <2926686459@qq.com>
2023-02-04 03:39:00 +0000

    回退 'Pull Request !16 : 陈新阳分支合并'

---
+++ firstapp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+import json
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+def test_land(request):
+    user_id = ['2022007184']
+    password = ['ycx666']
+    if request.method == 'POST':
+        print("11111")
+        test_name = request.POST.get('id')
+        test_password = request.POST.get('pw')
+        if test_name == user_id[0]:
+            if test_password != password[0]:
+                return HttpResponse(json.dumps({'user_id': '1', 'password': '0'}))
+            if test_password == password[0]:
+                return HttpResponse(json.dumps({'user_id': '1', 'password': '1'}))
+        if test_password != user_id[0]:
+            return HttpResponse(json.dumps({'user_id': '0', 'password': '0'}))
